2bdaeb00ef9a8c6b64266c103d5058bde2d8d570
avpadill <avpadill@ucalgary.ca>
2019-08-29 02:02:34 -0600

    Switching google map libraries, integrating with server, pinning retrieved locations.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 import React, { Component } from 'react';
+import SearchBox from './components/SearchBox';
 
-export class MapContainer extends Component {
+const MapComponent = withScriptjs(withGoogleMap((props) =>
+  <div>
+    <GoogleMap
+      defaultZoom={13}
+      defaultCenter={{ lat: 37.7749295, lng: -122.4194155 }}
+    >
+      {props.markers}
+    </GoogleMap>
+  </div>
+));
+
+class MapContainer extends Component {
+
+  state = {
+    markers : []
+  };
+
+  static defaultProps = {
+    center: {lat: 37.7749295, lng: -122.4194155},
+    zoom: 13
+  };
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
+  };
+
+  handleClick = () => {
+    console.log(`Got input: ${this.state.input}`);
+
+    fetch(`http://localhost:3001/locations?title=${this.state.input}`, {
+      cache: 'no-store',
+      credentials: 'omit'
+    }).then((result) => {
+
+      return result.json().then((data) => {
+
+        console.log('=== Got Data: ', data);
+
+        let retrievedLocations = data.Locations;
+
+        console.log('\n\n Locations: ', retrievedLocations);
+
+        this.setState({
+          markers: retrievedLocations
+        });
+
+
+      });
+
+    });
+  };
 
   render() {
-    return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>Testing</h1>
-            </div>
-        </InfoWindow>
-      </Map>
+
+    let markers = null;
+
+    markers = (
+      <div>
+        { this.state.markers.map((location) => {
+
+          console.log('\n\n === Testing: ', location.location);
+
+          return <Marker
+            position={location.location}
+            label= {location.name}
+          />
+
+        })}
+
+      </div>
     );
+
+    return (
+
+      <div>
+        <SearchBox 
+          handleChange={this.handleChange}
+          handleClick={this.handleClick}
+        />
+
+        <MapComponent
+          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDNk7in3pRazdod4nels3lv_7ElxZTtzvo&v=3.exp&libraries=geometry,drawing,places"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `800px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          markers={ markers }
+        />
+
+      </div>
+    );
+
   }
 }
- 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDNk7in3pRazdod4nels3lv_7ElxZTtzvo'
-})(MapContainer)
+
+export default MapContainer;
---
+++ src/components/SearchBox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchBox = (props) => {
+
+    const style = {
+        top: '10px',
+        padding: '5px',
+        border: '1px solid #999',
+        'text-align': 'center',
+        'line-height': '30px',
+    };
+
+    return (
+        <div className="SearchBox" style={style} >
+            <input type="text" onChange={props.handleChange} placeholder = {'Enter a movie title'} />
+            <button type="submit" onClick={props.handleClick} >Search</button>
+        </div>
+    );
+};
+
+export default SearchBox;
