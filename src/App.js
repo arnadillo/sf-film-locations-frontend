619c7deb704f30c55a391c9dbbab40645c6207dc
avpadill <avpadill@ucalgary.ca>
2019-08-28 19:28:10 -0600

    Using google-maps-react to get display map

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from 'react';
+
+export class MapContainer extends Component {
+
+  render() {
+    return (
+      <Map google={this.props.google} zoom={14}>
+ 
+        <Marker onClick={this.onMarkerClick}
+                name={'Current location'} />
+ 
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Testing</h1>
+            </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+ 
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDNk7in3pRazdod4nels3lv_7ElxZTtzvo'
+})(MapContainer)
